5b8d17c11118ae65d4477fe9b0f55b0d5f63a7e6
ethan-whiton <ethan.whiton@mymail.champlain.edu>
2026-02-26 14:17:12 -0500

    Got random enemy move selection and targeting working. The framework for more advanced AI is in place for when I can get around to it.

--- combat-system/scripts/units/enemy_unit.gd
+++ combat-system/scripts/units/enemy_unit.gd
@@ -1,6 +1,10 @@
 extends Unit
 class_name EnemyUnit
 
+enum AI_Type {RANDOM, SMART}
+
+@export var ai_type : AI_Type
+
 
 # =================================================================================================
 
@@ -21,21 +25,57 @@ func autolevel(levels_to_give: int):
 		spd_growth += increase_stat(spd_growth)
 		levels_to_give -= 1
 
-func random_move(target_list : Array[PlayerUnit]):
-	current_action = Action.MOVE
+func select_move():
+	match ai_type:
+		AI_Type.RANDOM:
+			while current_move == null:
+				var temp_move : Move = moves.pick_random()
+				if temp_move != null:
+					current_move = temp_move
+		_:
+			print("ERROR. Invalid AI type for " + unit_name + ".")
 	
-	var valid_move = false
-	while valid_move == false:
-		current_move = moves.pick_random()
-		if current_move != null:
-			valid_move = true
+	set_targeting(current_move)
 
-func select_move(): # This ai will get a massive overhaul at some point. I just want to get it functioning for now
-	# select a random move
-	while current_move != null:
-		var temp_move : Move = moves.pick_random()
-		if temp_move != null:
-			current_move = temp_move
-	# set targeting based on the move's targeting type. Make sure it can't target incapacitated guys
-	# yay!
+func set_targeting(move : Move):
+	match move.targeting_type:
+		Move.Targeting_Type.SINGLE:
+			# For now, choose randomly between the two. Eventually, pick highest damage.
+			var random = randi_range(0, 1)
+			if random == 0:
+				current_targets.append(combat_manager.player_primary)
+			else:
+				current_targets.append(combat_manager.player_secondary)
+		Move.Targeting_Type.SPREAD:
+			current_targets.append(combat_manager.player_primary)
+			current_targets.append(combat_manager.player_secondary)
+		Move.Targeting_Type.SELF:
+			if current_position == Position.PRIMARY:
+				current_targets.append(combat_manager.enemy_primary)
+			elif current_position == Position.SECONDARY:
+				current_targets.append(combat_manager.enemy_secondary)
+			else:
+				print("ERROR. " + unit_name + " is attempting to attack from the Backline position.")
+		Move.Targeting_Type.ALLY:
+			if current_position == Position.PRIMARY:
+				current_targets.append(combat_manager.enemy_secondary)
+			elif current_position == Position.SECONDARY:
+				current_targets.append(combat_manager.enemy_primary)
+			else:
+				print("ERROR. " + unit_name + " is attempting to attack from the Backline position.")
+		Move.Targeting_Type.TEAM:
+			current_targets.append(combat_manager.enemy_primary)
+			current_targets.append(combat_manager.enemy_secondary)
+		Move.Targeting_Type.FIELD:
+			current_targets.append(combat_manager.player_primary)
+			current_targets.append(combat_manager.player_secondary)
+			
+			if current_position == Position.PRIMARY:
+				current_targets.append(combat_manager.enemy_secondary)
+			elif current_position == Position.SECONDARY:
+				current_targets.append(combat_manager.enemy_primary)
+			else:
+				print("ERROR. " + unit_name + " is attempting to attack from the Backline position.")
+		_:
+			print("ERROR. Invalid targeting type for " + unit_name + "'s " + move.name)
 	pass
