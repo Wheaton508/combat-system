16d1f9a6d5caf57de4abe3893dde6ef61b3b2eb2
ethan-whiton <ethan.whiton@mymail.champlain.edu>
2026-04-13 19:42:07 -0400

    Class base stats added.

---
+++ combat-system/scenes/ui_manager.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+class_name UIManager
+
+var current_ui : Control
+# Maybe just make this a list??? A stack of ui to go back through???
+var previous_ui : Control
+
+
+# =================================================================================================
+
+
+func show_ui():
+	pass
+
+func go_to_previous():
+	pass
---
+++ combat-system/scripts/ui/target_info_box.gd
@@ -0,0 +1,9 @@
+extends Control
+class_name TargetInfoBox
+
+@export_group("Node References")
+@export var unit_name : Label
+@export var target_button : Button
+
+
+# =================================================================================================
