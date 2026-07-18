e254f8156d1ab1dd2a82a86267bbc702b5011194
Wheaton508 <ethandw300@gmail.com>
2026-07-18 00:56:47 -0400

    Equipping new moves or items now saves to the codex in the save file. Began work on stats logic.

--- infinite-arena/scripts/custom_resources/stats.gd
+++ infinite-arena/scripts/custom_resources/stats.gd
@@ -1,9 +1,31 @@
 class_name Stats
 extends Resource
 
+#region ACTUAL STATS
+var level : int
+var hp : int
+var atk : int
+var def : int
+var mag : int
+var res : int
+var spd : int
+#endregion
+
+#region GROWTH RATES
+var hp_growth : int
+var atk_growth : int
+var def_growth : int
+var mag_growth : int
+var res_growth : int
+var spd_growth : int
+#endregion
+
 
 # =================================================================================================
 
 
-func _initialize_stats(base_role : Role):
+func _initialize_stats(base_role : Role) -> void:
+	level = 1
+
+func _set_stat() -> void:
 	pass
--- infinite-arena/scripts/custom_resources/unit_data.gd
+++ infinite-arena/scripts/custom_resources/unit_data.gd
@@ -4,7 +4,7 @@ extends Resource
 @export var unit_name : String
 @export var unit_role : Role
 @export var unit_stats : Stats = Stats.new() # Initializes a new Stats resource on UnitData creation
-@export var move_list : Array[Move]
+@export var move_list : Array[Move] = [null, null, null, null]
 @export var hat : Equipment
 @export var top : Equipment
 @export var bottoms : Equipment
@@ -21,18 +21,15 @@ func _initialize_data():
 	
 	unit_stats._initialize_stats(unit_role)
 	
-	for m in unit_role.starting_moves:
-		pass
+	for m in unit_role.starting_moves.size():
+		_learn_move(unit_role.starting_moves[m], m)
 	
 	for e in unit_role.starting_equipment:
 		_equip_equipment(e)
 
 func _learn_move(pending_move : Move, move_slot : int) -> void:
 	move_list[move_slot] = pending_move
-	
-	var new : bool = _check_codex()
-	if new == true:
-		_update_codex()
+	_check_codex(pending_move)
 
 func _equip_equipment(pending_equip : Equipment) -> void:
 	match pending_equip.equip_type:
@@ -47,19 +44,29 @@ func _equip_equipment(pending_equip : Equipment) -> void:
 		Equipment.Equipment_Type.ACCESSORY:
 			pass
 	
-	var new : bool = _check_codex()
-	if new == true:
-		_update_codex()
+	_check_codex(pending_equip)
 
 func _equip_item(pending_item : Item) -> void:
-	
-	
-	var new : bool = _check_codex()
-	if new == true:
-		_update_codex()
+	item = pending_item
+	_check_codex(pending_item)
 
-func _check_codex() -> bool:
-	return true
-
-func _update_codex() -> void:
-	pass
+func _check_codex(item_to_check) -> void:
+	var save_game : SaveData = SafeResourceLoader.load(Global.save_manager.save_game_path) as SaveData
+	
+	if item_to_check is Move:
+		var move : Move = item_to_check as Move
+		if save_game.moves_dict[move.id] == false:
+			save_game.moves_dict[move.id] = true
+			ResourceSaver.save(save_game, Global.save_manager.save_game_path)
+	elif item_to_check is Equipment:
+		var equip : Equipment = item_to_check as Equipment
+		if save_game.equipment_dict[equip.id] == false:
+			save_game.equipment_dict[equip.id] = true
+			ResourceSaver.save(save_game, Global.save_manager.save_game_path)
+	elif item_to_check is Item:
+		var new_item : Item = item_to_check as Item
+		if save_game.item_dict[new_item.id] == false:
+			save_game.item_dict[new_item.id] = true
+			ResourceSaver.save(save_game, Global.save_manager.save_game_path)
+	else:
+		print_debug("ERROR. Item is not of a valid type.")
